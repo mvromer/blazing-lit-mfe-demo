b5a22614ba11e0a4b4b739c80087b365cda95186
Michael Romer <mvromer@gmail.com>
2023-05-19 23:18:59 -0500

    Fix typescript error

--- app-shell/src/main.ts
+++ app-shell/src/main.ts
@@ -14,6 +14,6 @@ const applications = constructApplications({
   }
 });
 
-const layoutEngine = constructLayoutEngine({ routes, applications });
+constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 start();
