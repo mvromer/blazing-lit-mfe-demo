31ad43c5c2b98e05ab4268a61e9d088d713a8827
Michael Romer <mvromer@gmail.com>
2023-05-20 22:30:32 -0500

    Add basic nav items

--- app-shell/src/main.ts
+++ app-shell/src/main.ts
@@ -1,19 +1,32 @@
-import { registerApplication, start } from 'single-spa';
+import { navigateToUrl, registerApplication, start } from 'single-spa';
 import {
   constructApplications,
   constructRoutes,
-  constructLayoutEngine
+  constructLayoutEngine,
 } from 'single-spa-layout';
 
-const appLayout = document.querySelector('#app-layout');
-const routes = constructRoutes(appLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return import(/* @vite-ignore */ `@mfe-demo/${name}`);
-  }
-});
+connectNavItems();
+loadMicroFrontends();
 
-constructLayoutEngine({ routes, applications });
-applications.forEach(registerApplication);
-start();
+function connectNavItems() {
+  document
+    .querySelectorAll('.mfe-nav-item')
+    .forEach((navItem) =>
+      navItem.addEventListener('click', (e) => navigateToUrl(e as any))
+    );
+}
+
+function loadMicroFrontends() {
+  const appLayout = document.querySelector('#app-layout');
+  const routes = constructRoutes(appLayout);
+  const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+      return import(/* @vite-ignore */ `@mfe-demo/${name}`);
+    },
+  });
+
+  constructLayoutEngine({ routes, applications });
+  applications.forEach(registerApplication);
+  start();
+}
