4d9f3fbdd1439bef58dd49aac8d8edff2639db09
Michael Romer <mvromer@gmail.com>
2024-02-14 00:40:02 -0600

    Fix custom element registration

--- orders/src/OrdersApp/Program.cs
+++ orders/src/OrdersApp/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrdersApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.RegisterCustomElement<App>("mfe-catalog-app");
+builder.RootComponents.RegisterCustomElement<App>("mfe-orders-app");
 
 builder.Services.AddScoped(
     sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 );
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
