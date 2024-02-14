c3301b197acd735baffc7dcb393d8c2e3f2fb746
Michael Romer <mvromer@gmail.com>
2024-02-14 00:48:19 -0600

    Add missing using

--- orders/src/OrdersApp/Program.cs
+++ orders/src/OrdersApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using OrdersApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
